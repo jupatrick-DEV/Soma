ed5078e6a478913e703d548bff010cf25dffa5c2
Juliana Patrick <>
2021-04-11 22:50:07 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int a, b, soma;
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &a); 
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &b); 
+  soma = a + b;
+   
+  printf("O valor da soma = %d\n", soma); 
+  
+   return(0);
+}
+
